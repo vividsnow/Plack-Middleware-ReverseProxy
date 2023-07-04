70d47b709bb2da513a0e792d08ab4f1d1f54e519
vividsnow <vividsnow@gmail.com>
2023-07-04 22:21:08 +0300

    truncated version

--- lib/Plack/Middleware/ReverseProxy.pm
+++ lib/Plack/Middleware/ReverseProxy.pm
@@ -7,64 +7,9 @@ use parent qw(Plack::Middleware);
 our $VERSION = '0.16';
 
 sub call {
-    my $self = shift;
-    my $env = shift;
-
-    # in apache2 httpd.conf (RequestHeader set X-Forwarded-HTTPS %{HTTPS}s)
-    $env->{HTTPS} = $env->{'HTTP_X_FORWARDED_HTTPS'}
-        if $env->{'HTTP_X_FORWARDED_HTTPS'};
-    $env->{HTTPS} = 'ON'
-        if $env->{'HTTP_X_FORWARDED_PROTO'} && $env->{'HTTP_X_FORWARDED_PROTO'} eq 'https';    # Pound
-    $env->{'psgi.url_scheme'}  = 'https' if $env->{HTTPS} && uc $env->{HTTPS} eq 'ON';
-    my $default_port = $env->{'psgi.url_scheme'} eq 'https' ? 443 : 80;
-
-
-    # If we are running as a backend server, the user will always appear
-    # as 127.0.0.1. Select the most recent upstream IP (last in the list)
-    if ( $env->{'HTTP_X_FORWARDED_FOR'} ) {
-        my ( $ip, ) = $env->{HTTP_X_FORWARDED_FOR} =~ /([^,\s]+)$/;
-        $env->{REMOTE_ADDR} = $ip;
-    }
-
-    if ( $env->{HTTP_X_FORWARDED_HOST} ) {
-
-        # in apache1 ServerName example.com:443
-        if ( $env->{HTTP_X_FORWARDED_SERVER} ) {
-            my ( $host, ) = $env->{HTTP_X_FORWARDED_SERVER} =~ /([^,\s]+)$/;
-            if ( $host =~ /^(.+):(\d+)$/ ) {
-#            $host = $1;
-                $env->{SERVER_PORT} = $2;
-                $env->{'psgi.url_scheme'}  = 'https' if $env->{SERVER_PORT} == 443;
-            }
-            $env->{HTTP_HOST} = $host;
-        }
-
-        my ( $host, ) = $env->{HTTP_X_FORWARDED_HOST} =~ /([^,\s]+)$/;
-        if ( $host =~ /^(.+):(\d+)$/ ) {
-#            $host = $1;
-            $env->{SERVER_PORT} = $2;
-        } elsif ( $env->{HTTP_X_FORWARDED_PORT} ) {
-            # in apache2 httpd.conf (RequestHeader set X-Forwarded-Port 8443)
-            $env->{SERVER_PORT} = $env->{HTTP_X_FORWARDED_PORT};
-            $host .= ":$env->{SERVER_PORT}";
-            $env->{'psgi.url_scheme'} = 'https'
-                if $env->{SERVER_PORT} == 443;
-        } else {
-            $env->{SERVER_PORT} = $default_port;
-        }
-        $env->{HTTP_HOST} = $host;
-
-    } elsif ( $env->{HTTP_HOST} ) {
-        my $host = $env->{HTTP_HOST};
-        if ($host =~ /^(.+):(\d+)$/ ) {
-#            $env->{HTTP_HOST}   = $1;
-            $env->{SERVER_PORT} = $2;
-        } elsif ($host =~ /^(.+)$/ ) {
-            $env->{HTTP_HOST}   = $1;
-            $env->{SERVER_PORT} = $default_port;
-        }
-    }
-
+    my ($self, $env) = @_;
+    $env->{'psgi.url_scheme'} = 'https' if $env->{HTTP_X_FORWARDED_PROTO} && $env->{HTTP_X_FORWARDED_PROTO} eq 'https';
+    $env->{REMOTE_ADDR} = ($env->{HTTP_X_FORWARDED_FOR} =~ /([^,\s]+)$/)[0] if $env->{HTTP_X_FORWARDED_FOR};
     $self->app->($env);
 }
 
